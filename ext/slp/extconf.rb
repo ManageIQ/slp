8608f3f265ff0aa97aab6588c8d28a9fc6c14157
Oleg Barenboim <chessbyte@gmail.com>
2015-06-18 14:33:30 -0400

    Added configuration and checking of SLP library

--- ext/slp/extconf.rb
+++ ext/slp/extconf.rb
@@ -1,3 +1,24 @@
 require 'mkmf'
 
-create_makefile("slp/slp");
+dir_config('slp')
+
+if Gem.win_platform?
+  $INCFLAGS << " -Iwin32 "
+  $CFLAGS.sub! /-MD/, "-MT"
+
+  slp_lib = "win32/slp"
+else
+  slp_lib = "slp"
+end
+
+unless have_header("slp.h")
+  puts "Could not find slp.h"
+  exit 1
+end
+
+unless have_library(slp_lib)
+  puts "Could not find library: #{slp_lib}"
+  exit 1
+end
+
+create_makefile("slp/slp")
